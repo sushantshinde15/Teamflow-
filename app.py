08b358bf05801208207ed2ac77d72dbd16550c7d
SUSHANT SHINDE <sushantcreates>
2026-05-18 15:37:50 +0530

    Fix deployment files

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from app import create_app, db, socketio
 
 app = create_app()
 
+
+with app.app_context():
+    db.create_all()
+    print("Database tables created!")
+
+# Run locally only
 if __name__ == '__main__':
-
-    with app.app_context():
-        db.create_all()
-        print("Database tables created!")
-
-    socketio.run(app, debug=True)
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=5000,
+        debug=True,
+        allow_unsafe_werkzeug=True
+    )
